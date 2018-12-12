67ba0788a464932703911b03afd35eae72c0941e
JasonZhang156 <zczhang156@163.com>
2018-12-12 13:50:19 +0800

    extract MFCC feature and Logmel spectrogram feature

--- esc10_extraction.py
+++ esc10_extraction.py
@@ -13,15 +13,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-def dense_to_one_hot(labels_dense, num_classes):
-    """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = np.arange(num_labels) * num_classes
-    labels_one_hot = np.zeros((num_labels, num_classes))
-    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-    return labels_one_hot
-
-
 def extract_esc10_feat():
     # 5-fold cross validation settings
     cv_index = np.load('cvindex.npz')
